59ce551fd002ccb0e06b5fb9e8402383eadf3200
pchmn <paul.chemin35@gmail.com>
2016-04-21 22:54:35 +0200

    modif wsgi.py

--- danzeenldc_api/danzeenldc_api/wsgi.py
+++ danzeenldc_api/danzeenldc_api/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "danzeenldc_api.settings")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "danzeenldc_api.danzeenldc_api.settings")
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
